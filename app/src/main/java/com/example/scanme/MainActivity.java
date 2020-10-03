7cf6d8efa12a78237fa0ec6b6de2b156f3b3a38b
pserdax <parahatgeldiyew.s@gmail.com>
2020-10-03 18:08:18 +0300

    Integrated Stetho (db monitorization purposes)

--- app/src/main/java/com/example/scanme/MainActivity.java
+++ app/src/main/java/com/example/scanme/MainActivity.java
@@ -9,7 +9,13 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.scanme.databinding.ActivityMainBinding;
+import com.facebook.stetho.DumperPluginsProvider;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.dumpapp.DumperPlugin;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialization of stetho facebook
+        Stetho.initializeWithDefaults(this);
     }
 }
